7c0fda00318b11c623ead564e24e196bc1775a5b
wangjp0702 <20612736@qq.com>
2015-06-09 17:35:13 +0800

    6-9

--- 奇看项目脚步工具/HttpHandle.py
+++ 奇看项目脚步工具/HttpHandle.py
@@ -38,8 +38,34 @@ class Request:
         #req.add_header('AccessKey',self.accessKey) 
         #req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
         #response = urllib.request.urlopen(req)
-        headers = {'AccessKey': self.accessKey}
+        headers = {'AccessKey': self.accessKey }
+
+    
         response=requests.post(self.url,files=files,headers=headers)
         
         return response.content.decode('UTF8')
-       
+    def RequestPostFileWithData( self ):
+        files = {'file':(self.filename, open(r''+self.filepath+'','rb'),self.contentType)}
+        #boundary = '----------%s' % hex(int(time.time() * 1000))
+        #data = []
+        #data.append('--%s' % boundary)
+
+        #fr1=open(r''+self.filepath+'','rb')
+        #data.append('Content-Disposition: form-data; name="%s"; filename="%s"' % ('file',self.filename))
+        #data.append('Content-Type: %s\r\n' % 'image/jpg')
+        #data.append(fr1.read())
+        #fr1.close()
+        #data.append('--%s--\r\n' % boundary)
+    
+        #http_body='\r\n'.join(data)
+        #req = urllib.request.Request(self.url,data=http_body.encode('utf-8'),method=self.method)
+        
+        #req.add_header('AccessKey',self.accessKey) 
+        #req.add_header('Content-Type', 'multipart/form-data; boundary=%s' % boundary)
+        #response = urllib.request.urlopen(req)
+        headers = {'AccessKey': self.accessKey }
+        data =self.data
+    
+        response=requests.post(self.url,data=data,files=files,headers=headers)
+        
+        return response.content.decode('UTF8')      
--- 奇看项目脚步工具/文章处理.py
+++ 奇看项目脚步工具/文章处理.py
@@ -1,23 +1,87 @@
+# coding: UTF-8
 import hashlib, hmac,json, urllib.request
-import HttpHandle,time,sys, re, json 
-def getImg(imgurl):
-    imgTag={"resource_url":imgurl,"resource_description":{"type":1,"image_size":500,"image_crop_rect":"0,0,", "image_width_rate":1}}
-    return json.dumps(imgTag)
+import HttpHandle,time,sys, re, json, HttpHandle 
+from PIL import Image  
+def getImg(imgname):
+    imagePath='D:\\qikan_data\\article\\1\\image\\'+imgname
+    image=Image.open(imagePath);
+    #im.format, im.size, im.mode
+    imginfo=str(image.size[0])+','+str(image.size[1])
+
+    appid='app_test_000000000000001'
+    userid='557225a7883c06080947864d'
+    token='5fd8669fdb82b1fb5b7cd53dc1aafa64e721267db6c38b3cab8961cfe472fa0ab17035e422c23565'
+    timestamp=int(time.time())
+    plaintext=userid+'|'+str(timestamp)
+    h=hmac.new('0123456789abcd0123456789'.encode('utf-8'),plaintext.encode('utf-8'),digestmod=hashlib.sha1)
+    accessKey= appid+'|'+token+'|'+h.hexdigest()+'|'+str(timestamp) 
+
+    values = {'resourceType':1}
+    url ='http://123.57.206.48:8080/item/resource'
+    #url='http://localhost:8080/channel/backgroundImage'
+    http=HttpHandle.Request()
+    http.url=url
+    http.method='POST'
+    http.data=values
+    http.filename=imgname
+    http.filepath=imagePath
+    http.contentType='image/'+image.format
+    http.accessKey=accessKey
+    jsonresult=''
+    jsonresult=json.loads(http.RequestPostFileWithData())
+    imgurl=jsonresult['url']
+    imgTag={"resource_url":imgurl,"resource_description":{"type":1,"image_size":imginfo,"image_crop_rect":"0,0,"+imginfo, "image_width_rate":1}}
+    return imgTag
+def getCoverImgID(imgname):
+    imagePath='D:\\qikan_data\\article\\1\\image\\'+imgname
+    image=Image.open(imagePath);
+    #im.format, im.size, im.mode
+    imginfo=str(image.size[0])+','+str(image.size[1])
+
+    appid='app_test_000000000000001'
+    userid='557225a7883c06080947864d'
+    token='5fd8669fdb82b1fb5b7cd53dc1aafa64e721267db6c38b3cab8961cfe472fa0ab17035e422c23565'
+    timestamp=int(time.time())
+    plaintext=userid+'|'+str(timestamp)
+    h=hmac.new('0123456789abcd0123456789'.encode('utf-8'),plaintext.encode('utf-8'),digestmod=hashlib.sha1)
+    accessKey= appid+'|'+token+'|'+h.hexdigest()+'|'+str(timestamp) 
+
+    values = {'resourceType':1}
+    url ='http://123.57.206.48:8080/item/resource'
+    #url='http://localhost:8080/channel/backgroundImage'
+    http=HttpHandle.Request()
+    http.url=url
+    http.method='POST'
+    http.data=values
+    http.filename=imgname
+    http.filepath=imagePath
+    http.contentType='image/'+image.format
+    http.accessKey=accessKey
+    jsonresult=''
+    jsonresult=json.loads(http.RequestPostFileWithData())
+    imgid=jsonresult['_id']
+    return imgid
 def getText(text):
     imgTag={"resource_description":{"type":2,"text":text,"text_alignment":0}}
-    return json.dumps(imgTag)
+    return imgTag
 
-url ='http://123.57.206.48:8080/register'
-urluser='http://123.57.206.48:8080/user'
-appid= 'app_test_000000000000001'
+import sys
+reload(sys)
+sys.setdefaultencoding('gb2312')
+#url ='http://123.57.206.48:8080/register'
+#urluser='http://123.57.206.48:8080/user'
+#appid= 'app_test_000000000000001'
+path="厘"
 
+#fr2 = open(path.decode('gbk').encode('utf8'))
 
 fr = open(r'D:\\qikan_data\\article\\1\1.txt','r+',-1,'utf-8')
+fw = open(r'D:\\qikan_data\\article\\1\2.txt','w+')
 #fwss = open(r'E:\\Code\?????????????????：???：???\????????????????：???：???\qikan_data\????.csv','w+')
 re_title = re.compile('<title>(.*)</title>')
 re_subtitle = re.compile('<subtitle>(.*)</subtitle>')
 re_cover = re.compile('<cover>(.*)</cover>')
-re_p = re.compile('<p>(.*)</P>')
+re_p = re.compile('<p>(.*)</p>')
 re_img = re.compile('<img>(.*)</img>')
 #re_title = re.compile('<title>(.*)</title>')
 #re_title = re.compile('<title>(.*)</title>')
@@ -25,10 +89,13 @@ re_img = re.compile('<img>(.*)</img>')
 title=""
 subtitle=""
 creator=""
-cover=""
+coverid=""
 content=""
 list=[]
+i=0
 for line in fr:
+    i+=1
+    print(i)
     m=re.search(re_title,line)
     if(m !=None):
         title=m.group(1)
@@ -39,7 +106,8 @@ for line in fr:
         continue
     m=re.search(re_cover,line)
     if(m !=None):
-        cover=m.group(1)
+        coverid=m.group(1)
+        coverid=getCoverImgID(coverid)
         continue
     m=re.search(re_p,line)
     if(m !=None):
@@ -54,6 +122,10 @@ for line in fr:
         list.append(r)
         continue
 
-print (list)
+data={"title":title,"subtitle":subtitle,"content":json.dumps(list,ensure_ascii=False),"converImageId":coverid}
+#print(data)
+fw.write(json.dumps(data,ensure_ascii=False))
+fw.flush()
+fw.close()
 #db.getCollection('users').find({nickName:/Appuser/})
 
